c3a2001c613b7a109939919198ae8e7856e99742
sebasarango1180 <sebas.arango@experimentality.co>
2017-12-01 00:21:40 -0500

    Added myself as an allowed subject.

--- pics_labeling.py
+++ pics_labeling.py
@@ -1,10 +1,10 @@
 import os
-path = "/home/experimentality/Documents/Inteligencia/final_inteligencia/Allowed/"
+path = "/home/experimentality/Documents/Inteligencia/final_inteligencia/Sebas/"
 files = os.listdir(path)
 i = 1
 
 for file in files:
 
     orig_name = file
-    os.rename(os.path.join(path, file), os.path.join(path, '1.alejo' + str(i) + '.jpg'))
+    os.rename(os.path.join(path, file), os.path.join(path, '1.sebas' + str(i) + '.jpg'))
     i = i + 1
